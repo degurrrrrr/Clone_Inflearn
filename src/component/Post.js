e1e884455434935189f7c56ce5afafe1c893c0d3
degurrrrrr <degurrrrrr@gmail.com>
2022-02-18 18:06:31 +0900

    post 작업 완료

--- src/component/Post.js
+++ src/component/Post.js
@@ -1,24 +1,103 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { Image } from "../element/index";
 
 const Post = (props) => {
+  return (
     <React.Fragment>
-        <div>
-            <div>
-             이미지   
-            </div>
-            <div>
-                제목
-            </div>
-            <div>
-                내용
-            </div>
-            <div>
-                
-            </div>
+      <DIV>
+        <Image height="40%" />
+        <div style={{ textAlign: "left", margin: "10px" }}>
+          <Contents>
+            <Title>
+              동해물과백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세
+            </Title>
+            <PostContent>
+              {" "}
+              동해물과백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세
+              동해물과백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세
+              동해물과백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세
+            </PostContent>
+          </Contents>
+          <div>
+            <PostTime>2022년 2월 18일 17:27:24</PostTime>
+            <Footer>
+              <Profile>
+                <Image shape="circle" size="30" />
+                <span>
+                  by<b> username</b>
+                </span>
+              </Profile>
+              <div>코멘트 0개</div>
+            </Footer>
+          </div>
         </div>
+      </DIV>
     </React.Fragment>
-}
+  );
+};
 
+const DIV = styled.div`
+  margin: 10px;
+  border: 1px solid #eee;
+  border-radius: 5px;
+  width: 350px;
+  height: 450px;
+  position: relative;
+  box-sizing: border-box;
+  &:hover {
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 5px 15px 0px;
+    -webkit-transform: scale(1.05);
+    -moz-transform: scale(1.05);
+    -ms-transform: scale(1.05);
+    -o-transform: scale(1.05);
+    transform: scale(1.05);
+  }
+`;
+const Title = styled.div`
+  white-space: initial;
+  font-size: 1rem;
+  margin: 0px 0px 0.25rem;
+  font-weight: bold;
+`;
+
+const Contents = styled.div`
+  /* white-space: initial; */
+  padding: 1rem;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+`;
+
+const PostContent = styled.div`
+  width: 296px;
+  height: 102px;
+  overflow: hidden;
+`;
+
+const PostTime = styled.div`
+  font-size: 0.75rem;
+  line-height: 1.5;
+  color: #ccc;
+  margin: 0rem 0rem 1rem 1rem;
+`;
+
+const Footer = styled.div`
+  font-size: 0.75rem;
+  display: inline-flex;
+  border-top: 1px solid #eee;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  width: 100%;
+  height: 50px;
+  /* position: absolute; */
+  bottom: 0px;
+`;
+
+const Profile = styled.div`
+  display: inline-flex;
+  align-items: center;
+`;
 
 export default Post;
--- src/element/Image.js
+++ src/element/Image.js
@@ -2,14 +2,24 @@ import React from "react";
 import styled from "styled-components";
 
 const Image = (props) => {
-  const { src, width, height, margin} = props;
+  const { size, src, width, height, margin, shape} = props;
 
   const styles = {
     src,
     width,
     height,
-    margin
+    margin,
+    shape,
+    size
   };
+
+
+  if (shape === 'circle') {
+    return (
+      //속성이 styles?
+      <ImageCircle {...styles}></ImageCircle>
+    );
+  }
 
   return (
     <>
@@ -22,14 +32,23 @@ Image.defaultProps = {
   src: "https://ilovecharacter.com/news/data/20210122/p179568629887999_597.jpg",
   width: '450px',
   height: '450px', 
+  size: 36,
   margin: false,
 };
 
+const ImageCircle = styled.div`
+--size: ${(props) => props.size}px;
+  width: var(--size);
+  height: var(--size);
+  border-radius: 18px;
+  background-image: url('${(props) => props.src}');
+  background-size: cover;
+  margin: 2px 4px;
+`;
+
 const ImageDefault = styled.div`
-  width: ${(props) => props.width}px;
-  min-width: 350px;
-  min-height: 300px;
-  height: ${(props) => props.height}px;
+ ${(props)=> props.width ? `height: ${props.width}`: ""};
+  ${(props)=> props.height ? `height: ${props.height}`: ""};
   ${(props)=> props.margin ? `margin: ${props.margin}`: ""};
   background-color: #59c1c2;
   background-image: url("${(props) => props.src}");
