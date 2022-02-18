cd9057d166df2b0144d9ef2f36c02d619fee8414
sws1552 <sws1552@gmail.com>
2022-02-18 15:54:07 +0900

    App.js backgroundcolor 삽입 및 header 만드는중

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,12 +1,27 @@
 
+import React from 'react';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <React.Fragment>
+      <Background>
+        
+      </Background>
+    </React.Fragment>
   );
 }
+
+
+const Background = styled.div`
+  min-height: 100vh;
+  height: 100%;
+  width: 100%;
+  background-color: #f8f9fa;
+`;
+
+
+
 
 export default App;
