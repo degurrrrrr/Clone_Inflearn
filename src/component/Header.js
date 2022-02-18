6e04f3673e1bff164bdf03909d9cd366bba36f0b
sws1552 <sws1552@gmail.com>
2022-02-18 18:35:45 +0900

    로그인전 헤더 완성

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Grid } from "../element";
 import LogoImg from "../images/velog 로고.png"
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = (props) => {
     return (
@@ -15,7 +16,9 @@ const Header = (props) => {
                         </a>
                     </Logo>
                     <HeadItem>
-
+                        <WbSunnyIcon style={{marginLeft: "20px"}}/>
+                        <SearchIcon style={{marginLeft: "20px"}} />
+                        <LoginBtn>로그인</LoginBtn>
                     </HeadItem>
                 </Wrap>
            </HeaderContainer>
@@ -46,9 +49,28 @@ const Logo = styled.div`
 `;
 
 const HeadItem = styled.div`
-
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    position: relative;
 `;
 
+const LoginBtn = styled.button`
+    margin-left: 20px;
+    height: 2rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    font-size: 1rem;
+    border-radius: 1rem;
+    border: none;
+    outline: none;
+    font-weight: bold;
+    word-break: keep-all;
+    background-color: black;
+    color: white;
+    transition: all 0.125s ease-in 0s;
+    cursor: pointer;
+`;
 
 
 export default Header;
